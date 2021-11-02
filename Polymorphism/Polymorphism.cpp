16269b7bf6bfa3b0022d8e10763e23aed764fcbc
Bahrd <przemyslaw.sliwinski@pwr.edu.pl>
2021-11-02 12:34:36 +0100

    Typing...

--- Polymorphism/Polymorphism.cpp
+++ Polymorphism/Polymorphism.cpp
@@ -1,7 +1,9 @@
 // Polymorphism.cpp : This file contains the 'main' function. Program execution begins and ends there.
 // https://docs.microsoft.com/en-us/troubleshoot/windows-client/deployment/dynamic-link-library
 
+#include <functional>
 #include <iostream>
+
 #include <windows.h>
 #include <WinUser.h>
 
@@ -9,8 +11,6 @@ extern "C"
 {
     int __declspec(dllimport)  SharedAdder(int, int);
 }
-//#include "..//Dll/dllmain.h"
-
 
 int main()
 {
@@ -18,20 +18,19 @@ int main()
               << SharedAdder(2, 2)
               << std::endl;
 
-
-    typedef int (*DLLPROC) (int, int);
-
+    using funT = int (*) (int, int);
     HINSTANCE hinstDLL;
-    DLLPROC sharedAdder;
     BOOL fFreeDLL = false;
-
+    
     hinstDLL = LoadLibrary(TEXT(R"(./Dll.dll)"));
     if (hinstDLL != NULL)
     {
-        sharedAdder = (DLLPROC)GetProcAddress(hinstDLL, "SharedAdder");
-        if (sharedAdder != NULL)
+        auto f = reinterpret_cast<funT>(GetProcAddress(hinstDLL, "SharedAdder"));
+        if (f != NULL)
+        {
             std::cout << "RT polymorphism: " 
-                      << sharedAdder(3, 3) << std::endl;
+                      << f(3, 3) << std::endl;
+        }
         fFreeDLL = FreeLibrary(hinstDLL);
     }
     return fFreeDLL == 0;
