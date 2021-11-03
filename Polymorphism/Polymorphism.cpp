aa013e50bec9511536ce2579a60f69bce95257a9
Bahrd <przemyslaw.sliwinski@pwr.edu.pl>
2021-11-03 13:23:37 +0100

    Messing around with types

--- Polymorphism/Polymorphism.cpp
+++ Polymorphism/Polymorphism.cpp
@@ -1,6 +1,7 @@
 // Polymorphism.cpp : This file contains the 'main' function. Program execution begins and ends there.
 // https://docs.microsoft.com/en-us/troubleshoot/windows-client/deployment/dynamic-link-library
 
+#include <future>
 #include <functional>
 #include <iostream>
 
@@ -17,20 +18,47 @@ extern "C"
 int main()
 {
     std::cout << "Basic LT polymorphism: "
-              << SharedAdder(2, 2)
+              << SharedAdder(1, 2)
               << std::endl;
 
-    using funT = int (*) (int, int);
     HINSTANCE hinstDLL = LoadLibrary(TEXT(R"(./Dll.dll)"));
     BOOL fFreeDLL = false;
     
     if (hinstDLL)
     {
         std::string name; std::cin >> name;
-        auto f = reinterpret_cast<funT>(GetProcAddress(hinstDLL, name.c_str()));
-        if (f)
-            std::cout << "Ultimate RT polymorphism: " 
-                      << f(3, 3) << std::endl;
+        {
+            using funT = int (*) (int, int);
+            auto f = reinterpret_cast<funT>(GetProcAddress(hinstDLL, name.c_str()));
+            if (f)
+            {
+                auto af = std::async(std::launch::deferred, f, 1, 2);
+
+                std::cout << "Ultimate RT polymorphism: "
+                    << f(1, 2) << std::endl
+                    << "Ultimate lazy RT polymorphism: "
+                    << af.get() << std::endl;
+
+            }
+        }
+        {
+            using funT = int (*) (int);
+            auto f = reinterpret_cast<funT>(GetProcAddress(hinstDLL, name.c_str()));
+            if (f)
+            {
+                std::cout << "FUBAR polymorphism: "
+                    << f(1) << std::endl;
+            }
+        }
+        {
+            using funT = int (*) (int, int, int);
+            auto f = reinterpret_cast<funT>(GetProcAddress(hinstDLL, name.c_str()));
+            if (f)
+            {
+                std::cout << "FUGAZI polymorphism: "
+                    << f(1, 2, 3) << std::endl;
+            }
+        }
         fFreeDLL = FreeLibrary(hinstDLL);
     }
     return fFreeDLL == 0;
