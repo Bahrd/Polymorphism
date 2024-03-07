2d5e07125405754d059ebc92fa24c1977aa78649
Przemysław Śliwiński, Ph.D., D.Sc. <Przemyslaw.M.Sliwinski@windowslive.com>
2024-03-07 22:37:49 +0100

    if RT else LT...

--- Dll/dllmain.cpp
+++ Dll/dllmain.cpp
@@ -1,30 +1,32 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
+// dumpbin.exe /EXPORTS .\Dll.dll
+
+
 #include "pch.h"
 #include "dllmain.h"
 
-#include <iostream>
 
-extern "C"
+template <typename T, typename U>
+auto LocalAdder(T t, U u) -> decltype(t + u)
 {
-    int SharedAdder(int a, int b)
-    {
-        return a + b;
-    }
+    return t + u;
 }
 
+extern "C" 
+{ 
+    int SharedAdder(int a, int b)
+    { 
+        return LocalAdder(a, b); 
+    } 
+}
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
 {
     switch (ul_reason_for_call)
     {
-        case DLL_PROCESS_ATTACH:
-        case DLL_THREAD_ATTACH:
-        case DLL_THREAD_DETACH:
-        case DLL_PROCESS_DETACH:
+        case DLL_PROCESS_ATTACH: case DLL_PROCESS_DETACH:
+        case DLL_THREAD_ATTACH:  case DLL_THREAD_DETACH:
         break;
     }
     return TRUE;
 }
-
-
-
--- Polymorphism/Polymorphism.cpp
+++ Polymorphism/Polymorphism.cpp
@@ -1,6 +1,11 @@
 // Polymorphism.cpp : This file contains the 'main' function. Program execution begins and ends there.
 // https://docs.microsoft.com/en-us/troubleshoot/windows-client/deployment/dynamic-link-library
-
+// https://docs.microsoft.com/en-us/cpp/build/walkthrough-creating-and-using-a-dynamic-link-library-cpp
+// About move semantics:
+// https://youtu.be/Bt3zcJZIalk?t=845
+// Hacker's kindergarden: 
+// https://en.wikipedia.org/wiki/Stack_buffer_overflow#Exploiting_stack_buffer_overflows
+// https://arstechnica.com/information-technology/2015/08/how-security-flaws-work-the-buffer-overflow/
 #include <future>
 #include <functional>
 #include <iostream>
@@ -9,41 +14,41 @@
 #include <windows.h>
 #include <WinUser.h>
 
-extern "C" int SharedAdder(int a, int b);
+extern "C" int SharedAdder(int, int);
 
 int main()
-{   
-    BOOL fFreeDLL = false;
-    
-    std::string bib, fun;
-    std::cin >> bib >> fun;
-    
-    auto wbib = LR"(.\)" + std::wstring(bib.begin(), bib.end()) + L".dll";
-    if (auto hinstDLL = LoadLibrary(wbib.c_str()))
-    {
-        if (auto _f = GetProcAddress(hinstDLL, fun.c_str()))
-        {
-            typedef unsigned int (*fun_t) (int, int);
+{    
+    std::string bib, fun; int a, b;
+    std::cin >> bib >> fun >> a >> b;  
 
-            auto f = reinterpret_cast<fun_t>(_f);
-            auto [bf, af] = std::tuple(std::bind(f, -1, 1), std::async(f, 1, -1));
+    auto wbib = LR"(.\)" + std::wstring(bib.begin(), bib.end()) + L".dll";
+    if (auto hinstDLL = LoadLibraryW(wbib.c_str()))
+    {
+        if (auto f = GetProcAddress(hinstDLL, fun.c_str()))
+        {
+            typedef int (*fun_t) (int, int);
+            auto tf = std::move(reinterpret_cast<fun_t>(f));
+            auto [bf, af] = std::pair(std::bind(tf, a, b), std::async(tf, a, b));
             
             std::cout << "Penultimate eager RT polymorphism: " << bf() << std::endl
-                      << "Ultimately lazy RT polymorphism: " << af.get() << std::endl;
+                      << "Ultimately RT lazy polymorphism: " << af.get() << std::endl;
         }
-        using fubar_t = int (*) (int);
+        using fubar_t = unsigned int (*) (int);
         if (auto f = reinterpret_cast<fubar_t>(GetProcAddress(hinstDLL, fun.c_str())))
         {
-            std::println("FUBAR polymorphism  I: {}", f(1));
-            std::cout << "FUBAR polymorphism II: " << f(-1) << std::endl;
+            std::println("FUBAR polymorphism  I: {}", f(a + b)),
+            std::cout << "FUBAR polymorphism II: " << f(a + b) << std::endl;
         }
-        using fugazi_t = long double (*) (int, int, std::string);
+        using fugazi_t = long double (*) (char, int, std::string);
         if (auto f = reinterpret_cast<fugazi_t>(GetProcAddress(hinstDLL, fun.c_str())))
         {
-            std::println(std::cout, "FUGAZI polymorphism: {}", f(-1, 1, "Have you ever tried this?"));
+            std::println(std::cout, "FUGAZI polymorphism: {}", f(a, b, "Have you ever tried this?"));
         }
-        fFreeDLL = FreeLibrary(hinstDLL);
+        return FreeLibrary(hinstDLL) == 0;
     }
-    std::cout << "Ol'good LT polymorphism: " << SharedAdder(1, -1) << std::endl;
-    return fFreeDLL == 0;
+    else
+    {
+        std::cout << "Ol'good LT polymorphism: " << SharedAdder(a, b) << std::endl;
+        return 0;
+    }
 }
